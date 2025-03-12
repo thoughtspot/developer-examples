944ed146a915df1cbb9398724336373ff8492df4
Prateek Agarwal <your-email-account@thoughtspot.com>
2025-03-12 11:41:52 +0530

    move init outside

--- visual-embed/visualization/src/App.tsx
+++ visual-embed/visualization/src/App.tsx
@@ -6,15 +6,15 @@ import Home from "./pages/Home";
 import EmbedViaClass from "./pages/EmbedViaClass";
 import EmbedViaReact from "./pages/EmbedViaReact";
 
-function App() {
+init({
+  thoughtSpotHost: import.meta.env.VITE_TS_HOST,
+  authType: AuthType.Basic,
+  username: import.meta.env.VITE_TS_USERNAME,
+  password: import.meta.env.VITE_TS_PASSWORD,
+  callPrefetch: true,
+});
 
-  init({
-    thoughtSpotHost: import.meta.env.VITE_TS_HOST,
-    authType: AuthType.Basic,
-    username: import.meta.env.VITE_TS_USERNAME,
-    password: import.meta.env.VITE_TS_PASSWORD,
-    callPrefetch: true,
-  });
+function App() {
 
   return (
     <>
