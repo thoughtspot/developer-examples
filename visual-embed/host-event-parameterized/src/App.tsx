e1e507d39467ce48a3548db4ac2a3e7127c3f969
sastaachar <justinjohnmathew100@gmail.com>
2025-03-14 01:00:35 +0530

    host-event-params : env

--- visual-embed/host-event-parameterized/src/App.tsx
+++ visual-embed/host-event-parameterized/src/App.tsx
@@ -1,38 +1,56 @@
 import tsLogo from "/ts-logo.svg";
 import "./App.css";
-import { AuthType, HostEvent, init } from "@thoughtspot/visual-embed-sdk";
+import {
+  AuthStatus,
+  AuthType,
+  HostEvent,
+} from "@thoughtspot/visual-embed-sdk";
 import {
   LiveboardEmbed,
   SearchEmbed,
   useEmbedRef,
+  useInit,
 } from "@thoughtspot/visual-embed-sdk/react";
+import { useEffect } from "react";
 
-init({
-  thoughtSpotHost: import.meta.env.VITE_THOUGHTSPOT_HOST,
-  authType: AuthType.Basic,
-  username: import.meta.env.VITE_THOUGHTSPOT_USERNAME,
-  password: import.meta.env.VITE_THOUGHTSPOT_PASSWORD,
-});
+const THOUGHTSPOT_LIVEBOARD_ID = import.meta.env.VITE_THOUGHTSPOT_LIVEBOARD_ID;
+const THOUGHTSPOT_VIZ_ID = import.meta.env.VITE_THOUGHTSPOT_VIZ_ID; 
 
 function App() {
+  const authEERef = useInit({
+    thoughtSpotHost: import.meta.env.VITE_THOUGHTSPOT_HOST,
+    authType: AuthType.Basic,
+    username: import.meta.env.VITE_THOUGHTSPOT_USERNAME,
+    password: import.meta.env.VITE_THOUGHTSPOT_PASSWORD,
+  });
+
+  useEffect(() => {
+    if (authEERef.current)
+      authEERef.current.on(AuthStatus.SDK_SUCCESS, () => {
+        console.log("SDK initialized successfully");
+      });
+  }, []);
+
   const liveboardRef = useEmbedRef<typeof LiveboardEmbed>();
   const searchRef = useEmbedRef<typeof SearchEmbed>();
 
   const handlePinForPinboard = async () => {
     const res = await liveboardRef.current.trigger(HostEvent.Pin, {
       newVizName: "Test Vziz",
-      vizId: "8d2e93ad-cae8-4c8e-a364-e7966a69a41e",
-      liveboardId: "6dabda7b-5392-4d47-ba26-17e8244cbcaf",
+      vizId: THOUGHTSPOT_VIZ_ID,
+      liveboardId: THOUGHTSPOT_LIVEBOARD_ID,
     });
-    console.log("Pin result (liveboard)" ,res);
+    console.log("Viz pinned to", res.liveboardId);
+    console.log("New viz id", res.liveboardId);
   };
 
   const handlePinForSearch = async () => {
     const res = await searchRef.current.trigger(HostEvent.Pin, {
       newVizName: "Test Vziz",
-      liveboardId: "6dabda7b-5392-4d47-ba26-17e8244cbcaf",
+      liveboardId: THOUGHTSPOT_LIVEBOARD_ID,
     });
-    console.log("Save result" ,res);
+    console.log("Viz pinned to", res.liveboardId);
+    console.log("New viz id", res.liveboardId);
   };
 
   const handleSaveFoSearch = async () => {
@@ -57,7 +75,10 @@ function App() {
           <div>
             <button onClick={handlePinForPinboard}> Pin params</button>
           </div>
-          <LiveboardEmbed liveboardId="e40c0727-01e6-49db-bb2f-5aa19661477b" ref={liveboardRef} />
+          <LiveboardEmbed
+            liveboardId="e40c0727-01e6-49db-bb2f-5aa19661477b"
+            ref={liveboardRef}
+          />
         </div>
         <div className="card">
           <h2>Search Embed</h2>
